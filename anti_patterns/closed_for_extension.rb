b0d7b0155278f00506733c6087595574373c7f97
Andrew Cox <drew.cox@gmail.com>
2011-01-28 22:02:27 -0500

    Expanded example and counter-example to Open Closed

--- anti_patterns/closed_for_extension.rb
+++ anti_patterns/closed_for_extension.rb
@@ -2,39 +2,60 @@
 # but closed for modification.
 #
 # The following implementation of a Story class simply prints the icon for each
-# type of story. It violates the Open Closed Principle because the icon
+# type of story.
+#
+# It violates the "closed for modification" principle because the icon
 # method must be modified in order to add a new type of story or to change
 # the icon of an existing story.
+#
+# It violates the "open for extension" principle because you must override
+# the stories method in order to change the order the stories are returned.
 #
 # In this situation, the case statement is a code smell that the method is
 # not open for extension.
 
-class Story
-  def initialize(type = "story")
-    @type = type
+module Agile
+  class Iteration
+    def initialize(options = {})
+      @stories = stories
+    end
+
+    def stories
+      @stories.sort {|a, b| a.description <=> b.description }
+    end
   end
 
-  def icon
-    case @type
-    when "bug" then "bug.png"
-    when "chore" then "gear.png"
-    else "star.png"
+  class Story
+    def initialize(options = {})
+      @type = options[:type] || "story"
+    end
+
+    def icon
+      case @type
+      when "bug" then "bug.png"
+      when "chore" then "gear.png"
+      else "star.png"
+      end
     end
   end
 end
 
-describe Story do
-  context "default story" do
-    it "has a star icon" do
-      story = Story.new
-      story.icon.should == "star.png"
-    end
-  end
+describe Agile do
+  include Agile
 
-  context "chore" do
-    it "has a gear icon" do
-      chore = Story.new("chore")
-      chore.icon.should == "gear.png"
+  describe Agile::Story do
+    context "default story" do
+      it "has a star icon" do
+        story = Story.new
+        story.icon.should == "star.png"
+      end
+    end
+
+    context "chore" do
+      it "has a gear icon" do
+        chore = Story.new(:type => "chore")
+        chore.icon.should == "gear.png"
+      end
     end
   end
 end
--- solid_principles/open_closed_principle.rb
+++ solid_principles/open_closed_principle.rb
@@ -1,30 +1,101 @@
 # The Open Closed principle states that a module should be open for extension
 # but closed for modification.
+#
+# It is open for extension in the ordering of stories in an iteration. You
+# can change how stories are ordered simply by overriding the <=> method
+# in the Story class.
+#
+# It is closed for modification in that you should not need to modify how
+# a Story determines its icon (see anti_patterns/closed_for_extension.rb
+# for a violation of the closed for modification principle).
 
-class Story
-  def icon
-    "star.png"
-  end
-end
+require "date"
 
-class Chore < Story
-  def icon
-    "gear.png"
-  end
-end
+module Agile
+  class Iteration
+    def initialize(stories = [])
+      @stories = stories
+    end
 
-describe Story do
-  context "default story" do
-    it "has a star icon" do
-      story = Story.new
-      story.icon.should == "star.png"
+    def stories
+      @stories.sort
     end
   end
 
-  context "chore" do
-    it "has a gear icon" do
-      chore = Chore.new
-      chore.icon.should == "gear.png"
+  class Story
+    attr_reader :description, :icon
+
+    def initialize(options = {})
+      @description = options[:description]
+      @icon = options[:icon] || "star.png"
+    end
+
+    def <=>(b)
+      0
+    end
+  end
+
+  class Release < Story
+    attr_reader :deadline
+
+    def initialize(options = {})
+      @deadline = options[:deadline]
+    end
+
+    def deadline
+      @deadline unless @deadline.nil?
+    end
+  end
+end
+
+describe Agile do
+  include Agile
+
+  describe Agile::Story do
+
+    context "default story" do
+      it "has a description" do
+        expected_description = "Write an example of Open Closed"
+        story = Story.new(:description => expected_description)
+        story.description.should == expected_description
+      end
+
+      it "has a star icon" do
+        story = Story.new
+        story.icon.should == "star.png"
+      end
+    end
+
+    context "chore" do
+      it "has a gear icon" do
+        chore = Story.new(:icon => "gear.png")
+        chore.icon.should == "gear.png"
+      end
+    end
+
+    context "release" do
+      it "can have a deadline" do
+        expected_deadline = Date.parse('2011-01-31')
+        release = Release.new(
+          :icon => "release.png",
+          :deadline => expected_deadline
+        )
+        release.deadline.should == expected_deadline
+      end
+    end
+  end
+
+  describe Agile::Iteration do
+    it "returns a list of stories in the order they were added" do
+      upcoming = Iteration.new([
+        Story.new(:description => "alpha"),
+        Story.new(:description => "theta"),
+        Story.new(:description => "beta")
+      ])
+      stories = upcoming.stories
+      stories[0].description.should == "alpha"
+      stories[1].description.should == "theta"
+      stories[2].description.should == "beta"
     end
   end
 end
