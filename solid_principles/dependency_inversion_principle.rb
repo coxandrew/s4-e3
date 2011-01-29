ce58d8a425ff4296850ceca7ecfe39abdfd5cfc4
Andrew Cox <drew.cox@gmail.com>
2011-01-29 11:21:02 -0500

    Moved t_shirt_size logic into Story and added HTML Printer example

--- solid_principles/dependency_inversion_principle.rb
+++ solid_principles/dependency_inversion_principle.rb
@@ -14,43 +14,60 @@
 #
 # This example uses Dependency Inversion to define the printer in the
 # initialize method. That allows us to be flexible with the mechanism
-# used for printing a story.
+# used for printing a story and print a story as either text or HTML.
 #
 
-class Story
-  attr_reader :description, :size
+module Agile
+  class Story
+    attr_reader :description, :size
 
-  def initialize(options = {})
-    @printer     = options[:printer]     || StoryPrinter.new
-    @description = options[:description] || "No description"
-    @size        = options[:size]        || 0
+    def initialize(options = {})
+      @printer     = options[:printer]     || StoryPrinter.new
+      @description = options[:description] || "No description"
+      @size        = options[:size]        || 0
+    end
+
+    def t_shirt_size
+      case size
+      when 1 then :XS
+      when 2 then :S
+      when 4 then :M
+      when 8 then :L
+      end
+    end
+
+    def print
+      @printer.print(self)
+    end
   end
 
-  def print
-    @printer.print(self)
-  end
-end
-
-class StoryPrinter
-  def print(story)
-    "#{story.description} (#{t_shirt_size(story.size)})"
+  class StoryPrinter
+    def print(story)
+      "#{story.description} (#{story.t_shirt_size})"
+    end
   end
 
-  private
-
-  def t_shirt_size(size)
-    case size
-    when 1 then "XS"
-    when 2 then "S"
-    when 4 then "M"
-    when 8 then "L"
+  class StoryHTMLPrinter
+    def print(story)
+      "<div class='story'>#{story.description} <em>(#{story.t_shirt_size})</em></div>"
     end
   end
 end
 
-describe Story do
-  it "prints a story" do
-    story = Story.new(:description => "alpha", :size => 4)
+describe Agile::Story do
+  it "prints a story as a string by default" do
+    story = Agile::Story.new(:description => "alpha", :size => 4)
     story.print.should == "alpha (M)"
+  end
+
+  context "StoryHTMLPrinter" do
+    it "prints a story as HTML" do
+      story = Agile::Story.new(
+        :description => "alpha",
+        :size => 8,
+        :printer => Agile::StoryHTMLPrinter.new
+      )
+      story.print.should == "<div class='story'>alpha <em>(L)</em></div>"
+    end
   end
 end
