f31ed1644d115512686ad0b87a227ee1d8bfc1bb
Andrew Cox <drew.cox@gmail.com>
2011-01-29 11:57:26 -0500

    Fleshed out example using respond_to? to determine what to print

--- solid_principles/interface_segregation_principle.rb
+++ solid_principles/interface_segregation_principle.rb
@@ -4,22 +4,75 @@
 # Clients should not be forced to depend on interfaces that they do not use.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
-# The Interface Segregation Principle does not apply to Ruby. You depend on the
-# signature of the method and otherwise, the object is a "duck".
+# Since Ruby is a dynamic language, you rely on duck typing to determine
+# what you can do with an object. In this example, the StoryPrinter object
+# is able to print a story or release with or without a deadline.
+#
+# StoryPrinter knows to print the deadline only if the object has that method.
+#
 
-class Story
-  def initialize(options = {})
-    @description = options[:description]
+module Agile
+  require "date"
+
+  class Story
+    attr_reader :description
+
+    def initialize(options = {})
+      @description = options[:description]
+      on_story_create(options)
+    end
+
+    def on_story_create(options)
+    end
   end
 
-  def description
-    @description
+  class Release < Story
+    attr_reader :deadline
+
+    def on_story_create(options)
+      @deadline = options[:deadline]
+    end
+  end
+
+  class StoryPrinter
+    def print(story)
+      formatted_deadline = ""
+      if story.respond_to?(:deadline) && !story.deadline.nil?
+        formatted_deadline = " (#{story.deadline.strftime('%b %d, %Y')})"
+      end
+      "#{story.description}" + formatted_deadline
+    end
   end
 end
 
-describe Story do
-  it "should have a description" do
-    story = Story.new(:description => "alpha")
-    story.description.should == "alpha"
+describe Agile do
+  let (:printer) { Agile::StoryPrinter.new }
+
+  describe Agile::Story do
+    it "prints a story" do
+      description = "RMU Session 4"
+      story = Agile::Story.new(:description => description)
+      printer.print(story).should == "#{description}"
+    end
+  end
+
+  describe Agile::Release do
+    it "prints a story without a deadline" do
+      description = "RMU Session 4"
+      story = Agile::Release.new(
+        :description => description
+      )
+      printer.print(story).should == description
+    end
+
+    it "prints a story with a deadline" do
+      description = "RMU Session 4"
+      jan_31 = Date.parse("2011-01-31")
+      story = Agile::Release.new(
+        :description => description,
+        :deadline => jan_31
+      )
+      printer.print(story).should == "#{description} (Jan 31, 2011)"
+    end
   end
 end
